e330fcccd7e1ea6475644360ba4c91359662bcf2
M0H1K4N <nejer911@gmail.com>
2024-02-01 23:53:12 +0400

    add

--- src/components/loginSignup/LoginSignup.js
+++ src/components/loginSignup/LoginSignup.js
@@ -1,11 +1,47 @@
-import React from 'react'
+import React from "react";
+import "./loginsignup.css";
+import { FaUser, FaEnvelope, FaPhone, FaLock } from 'react-icons/fa';
 
 const LoginSignup = () => {
   return (
-    <div>
-      hello this is sign form
+    <div className="wrapper">
+      <form action="">
+        <h1>Registration</h1>
+        <div className="input-box">
+          <div className="input-field">
+            <input type="text" placeholder="Full Name" required />
+            <FaUser size={20} />
+          </div>
+          <div className="input-field">
+            <input type="text" placeholder="Username" required />
+            <FaUser size={20} />
+          </div>
+          <div className="input-field">
+            <input type="email" placeholder="Email" required />
+            <FaEnvelope size={20} />
+          </div>
+          <div className="input-field">
+            <input type="number" placeholder="Phone Number" required />
+            <FaPhone size={20} />
+          </div>
+          <div className="input-field">
+            <input type="password" placeholder="Password" required />
+            <FaLock size={20} />
+          </div>
+          <div className="input-field">
+            <input type="password" placeholder="Confirm Password" required />
+            <FaLock size={20} />
+          </div>
+        </div>
+          <label>
+            <input type="checkbox"/>
+            I herebly declare that the above information provided is true and correct.
+          </label>
+
+          <button type="submit" className="btn">Register</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
 export default LoginSignup;

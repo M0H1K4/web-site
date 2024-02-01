6de2bcbd4e96a43874146637a74abc3f807dcf86
M0H1K4N <nejer911@gmail.com>
2024-02-02 01:16:54 +0400

    add

--- src/components/loginSignup/LoginSignup.js
+++ src/components/loginSignup/LoginSignup.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./loginsignup.css";
-import { FaUser, FaEnvelope, FaPhone, FaLock } from 'react-icons/fa';
+import { FaUser, FaEnvelope, FaPhone, FaLock } from "react-icons/fa";
 
 const LoginSignup = () => {
+  const setBodyBackgroundImage = () => {
+    document.body.style.backgroundImage = 'url("/image.jpg")'; // Replace with the URL of your image
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundPosition = "center";
+    document.body.style.backgroundRepeat = "no-repeat";
+  };
+  useEffect(() => {
+    setBodyBackgroundImage();
+  }, []);
+
   return (
     <div className="wrapper">
       <form action="">
@@ -10,35 +20,37 @@ const LoginSignup = () => {
         <div className="input-box">
           <div className="input-field">
             <input type="text" placeholder="Full Name" required />
-            <FaUser size={20} />
+            <FaUser size={20} className="icon" />
           </div>
           <div className="input-field">
             <input type="text" placeholder="Username" required />
-            <FaUser size={20} />
+            <FaUser size={20} className="icon" />
           </div>
           <div className="input-field">
             <input type="email" placeholder="Email" required />
-            <FaEnvelope size={20} />
+            <FaEnvelope size={20} className="icon" />
           </div>
           <div className="input-field">
             <input type="number" placeholder="Phone Number" required />
-            <FaPhone size={20} />
+            <FaPhone size={20} className="icon" />
           </div>
           <div className="input-field">
             <input type="password" placeholder="Password" required />
-            <FaLock size={20} />
+            <FaLock size={20} className="icon" />
           </div>
           <div className="input-field">
             <input type="password" placeholder="Confirm Password" required />
-            <FaLock size={20} />
+            <FaLock size={20} className="icon" />
           </div>
         </div>
-          <label>
-            <input type="checkbox"/>
-            I herebly declare that the above information provided is true and correct.
-          </label>
+        <label>
+          <input type="checkbox" />I herebly declare that the above information
+          provided is true and correct.
+        </label>
 
-          <button type="submit" className="btn">Register</button>
+        <button type="submit" className="btn">
+          Register
+        </button>
       </form>
     </div>
   );
